b186d0c1f62148fb8e8af5f1bc2774b73d64fb4d
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-03-01 01:58:16 +0100

    Abstract out logic to check auth and permissions into context

--- graphql/index.js
+++ graphql/index.js
@@ -11,16 +11,34 @@ const BookModel = require("../models/Book");
 
 const isTokenValid = require("../validate");
 
-const { errorCodes, errorTypes } = require("../errorHandler");
+const { RequestError, errorCodes, errorTypes } = require("../errorHandler");
 
 const context = async (req) => {
+	const { authorization: token } = req.headers;
 	const db = {
 		AuthorModel,
 		BookModel,
 	};
-	const { authorization: token } = req.headers;
 
-	return { db, token, isTokenValid };
+	const requiresAuth = async (permission = "") => {
+		/* 
+			Check that user's token is valid and they have the permission to perform an action. 
+			This function throws an error otherwise.
+		*/
+		const { error, decoded } = await isTokenValid(token);
+		if (error) {
+			throw new RequestError(error, errorTypes.UNAUTHORIZED);
+		}
+
+		if (permission) {
+			const permitted = decoded?.permissions.includes(permission); // check that the user is permitted to access this endpoint
+			if (!permitted) {
+				throw new RequestError("Permission not found", errorTypes.FORBIDDEN);
+			}
+		}
+	};
+
+	return { db, requiresAuth };
 };
 
 const schema = makeExecutableSchema({
--- graphql/resolvers/author/authors.js
+++ graphql/resolvers/author/authors.js
@@ -1,26 +1,7 @@
-const { RequestError, errorTypes } = require("../../../errorHandler");
-
 const authors = async (parent, args, context, info) => {
+	const { db, requiresAuth } = await context(); // get methods from context
 	const permission = "get:authors"; // required permission to access this endpoint
-
-	const { db, token, isTokenValid } = await context(); // get methods from context
-
-	/* 
-		Check that token is valid. 
-		This promise returns an error if invalid and a decoded jwt if valid.
-		Valid tokens will have permissions 
-	*/
-	const { error, decoded } = await isTokenValid(token);
-
-	if (error) {
-		throw new RequestError(error, errorTypes.UNAUTHORIZED);
-	}
-
-	const permitted = decoded?.permissions.includes(permission); // check that the user is permitted to access this endpoint
-
-	if (!permitted) {
-		throw new RequestError("Permission not found", errorTypes.FORBIDDEN);
-	}
+	await requiresAuth(permission);
 
 	let data = new db.AuthorModel().findAll();
 	return data;
