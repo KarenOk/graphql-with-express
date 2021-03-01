3ad5311c32975f734f54167c427ece7a648e99f5
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-03-01 01:02:24 +0100

    Implement auth checks for get authors
    Authentication and authorization checks with auth0

--- graphql/index.js
+++ graphql/index.js
@@ -6,14 +6,30 @@ const allQueries = require("./queries"); // All Exported Queries
 const allMutation = require("./mutations"); // All Exported Mutations
 const resolvers = require("./resolvers"); // All Exported Resolvers
 
+const AuthorModel = require("../models/Author");
+const BookModel = require("../models/Book");
+
+const isTokenValid = require("../validate");
+
+const context = async (req) => {
+	const db = {
+		AuthorModel,
+		BookModel,
+	};
+	const { authorization: token } = req.headers;
+
+	return { db, token, isTokenValid };
+};
+
 const schema = makeExecutableSchema({
 	typeDefs: `${allQueries} ${allMutation} ${allTypes}`,
 	resolvers,
 });
 
-const expressGraphQLMiddleware = expressGraphQl({
+const expressGraphQLMiddleware = expressGraphQl(async (req) => ({
 	schema: schema, // Build schema
 	graphiql: true,
-});
+	context: () => context(req),
+}));
 
 module.exports = expressGraphQLMiddleware; //Exporting graphQl method
--- graphql/resolvers/author/authors.js
+++ graphql/resolvers/author/authors.js
@@ -1,7 +1,22 @@
-const AuthorModel = require("../../../models/Author");
+const authors = async (parent, args, context, info) => {
+	const permission = "get:authors"; // required permission to access this endpoint
 
-const authors = (parent, args, context, info) => {
-	let data = new AuthorModel().findAll();
+	const { db, token, isTokenValid } = await context(); // get methods from context
+
+	/* 
+		Check that token is valid. 
+		This promise returns an error if invalid and a decoded jwt if valid.
+		Valid tokens will have permissions 
+	*/
+	const { error, decoded } = await isTokenValid(token);
+
+	if (error) {
+		throw new Error(error); // Throw error for invalid token
+	}
+
+	const permitted = decoded?.permissions.includes(permission); // check that the user is permitted to access this endpoint
+
+	let data = new db.AuthorModel().findAll();
 	return data;
 };
 
